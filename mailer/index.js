5bd160cf761b9ed293f3a8ceb9e6b4d7e0af9bd4
Adrian Kunz <clashsoft@hotmail.com>
2020-06-03 23:21:21 +0200

    Move style and js to separate files

---
+++ mailer/index.js
@@ -0,0 +1,51 @@
+const subject = document.getElementById('subject');
+const template = document.getElementById('template');
+const addresses = document.getElementById('addresses');
+const variables = document.getElementById('variables');
+const mailtoList = document.getElementById('mailto-list');
+
+subject.value = localStorage.getItem('subjectTemplate') || '';
+template.value = localStorage.getItem('bodyTemplate') || '';
+addresses.value = localStorage.getItem('addresses') || '';
+variables.value = localStorage.getItem('variables') || '';
+
+update();
+
+function replaceVariables(body, variables) {
+	for (let i = 0; i < variables.length; i++) {
+		body = body.replace(`#${i + 1}#`, variables[i] || '');
+	}
+	return body;
+}
+
+function update() {
+	const subjectTemplate = subject.value || '';
+	const bodyTemplate = template.value || '';
+	const addressesText = addresses.value || '';
+	const variablesText = variables.value || '';
+
+	localStorage.setItem('subjectTemplate', subjectTemplate);
+	localStorage.setItem('bodyTemplate', bodyTemplate);
+	localStorage.setItem('addresses', addressesText);
+	localStorage.setItem('variables', variablesText);
+
+	const addressList = addressesText.split('\n');
+	const variableList = variablesText.split('\n');
+
+	const maxRows = Math.max(addressList.length, variableList.length);
+	addresses.rows = maxRows;
+	variables.rows = maxRows;
+
+	let mailtoLinks = '';
+	for (let i = 0; i < addressList.length; i++) {
+		const recipient = addressList[i];
+		const variables = (variableList[i] || '').split('\t');
+
+		const subject = replaceVariables(subjectTemplate, variables);
+		const body = replaceVariables(bodyTemplate, variables);
+
+		mailtoLinks += `<a href="mailto:${recipient}?subject=${encodeURIComponent(
+			subject)}&body=${encodeURIComponent(body)}">mailto:${recipient}</a><br/>\n`;
+	}
+	mailtoList.innerHTML = mailtoLinks;
+}
